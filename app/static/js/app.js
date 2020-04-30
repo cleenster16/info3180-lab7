d0e7f11dc5bb339ac2349b70084c11f62d2c8c53
cleenster16 <cleenster16@yahoo.com>
2020-04-29 20:44:43 -0500

    Create front-end using VueJS

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,9 +9,12 @@ Vue.component('app-header', {
     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+            <li class="nav-item active">
             <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-          </li>
+            </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/upload">Upload</router-link>
+            </li>
         </ul>
       </div>
     </nav>
@@ -40,6 +43,71 @@ const Home = Vue.component('home', {
     }
 });
 
+const Upload = Vue.component('upload-form', {
+    template: `
+    <div class="form-control">
+        <h1>Upload Photo</h1>
+        <div v-if="message !== '' && message !== undefined" class="alert alert-success" role="alert">{{ message }}</div>
+        <div v-else-if="errors.length !== 0" class="alert alert-danger" role="alert">
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </div>
+        <form method="POST" id="uploadForm" @submit.prevent="uploadPhoto" enctype="multipart/form-data" class="">
+            <div>
+                <label for="description">Description</label>
+                <textarea name="description" id="description"></textarea>
+            </div>
+            <div>
+                <label for="photo">Photo</label>
+                <input id="photo" type="file" name='photo'/>
+            </div>
+            <button class="btn btn-primary" my-4 type="submit" name="submit">Submit</button>
+        </form>
+    </div>
+    `,
+    methods: {
+        uploadPhoto: function(){
+            let self = this;
+            let uploadForm = document.getElementById('uploadForm');
+            let form_data = new FormData(uploadForm);
+
+            self.message = "";
+            self.errors = [];
+            fetch("/api/upload", {
+                method: 'POST',
+                body : form_data,
+                headers: {
+                    'X-CSRFToken': token
+                },
+                credentials: 'same-origin'
+            })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (jsonResponse) {
+                    // display a success message
+                    if (jsonResponse.errors) {
+                        self.errors = jsonResponse.errors;
+                    } else {
+                        self.message = jsonResponse.message;
+                    }
+                    console.log(jsonResponse);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    },
+    data: function() {
+        return {
+            message: '',
+            errors: []
+        }
+    }
+
+});
+
 const NotFound = Vue.component('not-found', {
     template: `
     <div>
@@ -57,7 +125,7 @@ const router = new VueRouter({
     routes: [
         {path: "/", component: Home},
         // Put other routes here
-
+        {path: '/upload', component: Upload},
         // This is a catch all route in case none of the above matches
         {path: "*", component: NotFound}
     ]
--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import os
 from app import app
 from flask import render_template, request, jsonify
 from app.forms import UploadForm
@@ -17,13 +17,13 @@ from werkzeug.utils import secure_filename
 @app.route('/api/upload', methods=['POST'])
 def upload():
     form = UploadForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    if form.validate_on_submit():
         description = form.description.data
         photo = form.photo.data
         filename = secure_filename(photo.filename)
         photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-        return jsonify(message="200 - OK. File uploaded successfully.", filename=filename ,description=description)
-    return jsonify(errors=form_errors())
+        return jsonify(message="200 - OK. File uploaded successfully.", filename=filename, description=description)
+    return jsonify(errors=form_errors(form))
 
 # Please create all new routes and view functions above this route.
 # This route is now our catch all route for our VueJS single page
